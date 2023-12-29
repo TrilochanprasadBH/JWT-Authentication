e2df56a7d521d328761bb459e64108e60f63f3a7
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-12-29 11:28:24 +0530

    fixed error

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose= require("mongoose");
 require("dotenv").config()
 
-const connection= mongoose.connect(process.env.mongoUrl, {useNewUrlParser: true});
+const connection= mongoose.connect(process.env.mongoUrl);
 
 
 module.exports={
